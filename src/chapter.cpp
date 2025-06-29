56281033c683a8ef281ad010f7a3a259afa56a51
Kartikey Subramanium <kartikey.sub@outlook.com>
2025-06-29 22:03:57 +0530

    feat(backend): add features to Chapter API

--- src/chapter.cpp
+++ src/chapter.cpp
@@ -3,13 +3,26 @@
 #include <QFileInfo>
 
 #include "cbz.hpp"
+#include "image.hpp"
 
 Chapter::Chapter(QString path, QObject* parent)
     : QObject(parent),
       m_title(QFileInfo{path}.baseName()) {}
 
+Chapter::~Chapter() {
+  close();
+}
+
 const QString& Chapter::title() const {
   return m_title;
+}
+
+void Chapter::open() {}
+
+void Chapter::close() {}
+
+qsizetype Chapter::length() const {
+  return 0;
 }
 
 Chapter::Ptr Chapter::make(QString path, QObject* parent) {
@@ -21,6 +34,48 @@ Chapter::Ptr Chapter::make(QString path, QObject* parent) {
   }
 
   return std::make_unique<Chapter>(path, parent);
+}
+
+QImage Chapter::cover() const {
+  return get(0);
+}
+
+QImage Chapter::get(qsizetype) const {
+  return QImage{};
+}
+
+QUrl Chapter::url(qsizetype index) const {
+  const auto fmt = QString{u"image://%1/%2"};
+  if (index < length()) {
+    return QUrl(fmt.arg(IMAGE_PREFIX, QString::number(index)));
+  }
+  return QUrl{};
+}
+
+QUrl Chapter::coverUrl() const {
+  return url(0);
+}
+
+qsizetype Chapter::index() const {
+  return m_index;
+}
+
+QUrl Chapter::currentPage() const {
+  return url(index());
+}
+
+void Chapter::nextPage() {
+  if (index() < length()) {
+    m_index++;
+    Q_EMIT indexChanged();
+  }
+}
+
+void Chapter::prevPage() {
+  if (0 < index()) {
+    m_index--;
+    Q_EMIT indexChanged();
+  }
 }
 
 #include "moc_chapter.cpp"
--- src/chapter.hpp
+++ src/chapter.hpp
@@ -2,9 +2,12 @@
 #define HK_KOMIKMAN_CHAPTER_HPP
 
 #include <QDebug>
+#include <QImage>
 #include <QList>
 #include <QObject>
 #include <QString>
+#include <QUrl>
+#include <QtTypes>
 
 #include <memory>
 
@@ -17,20 +20,39 @@ class Chapter : public QObject {
 
  private:
   Q_PROPERTY(QString title MEMBER m_title CONSTANT)
+  Q_PROPERTY(QUrl currentPage READ currentPage NOTIFY indexChanged)
+  Q_PROPERTY(qsizetype index MEMBER m_index NOTIFY indexChanged)
+  Q_PROPERTY(qsizetype length READ length CONSTANT)
 
  public:
   explicit Chapter(QString path, QObject* parent = nullptr);
-  virtual ~Chapter() = default;
+  virtual ~Chapter();
 
   const QString& title() const;
 
-  virtual void open() {}
-  virtual void close() {}
+  virtual void open();
+  virtual void close();
+
+  virtual qsizetype length() const;
+
+  virtual QImage get(qsizetype) const;
+  QImage cover() const;
+
+  Q_INVOKABLE QUrl url(qsizetype index) const;
+  Q_INVOKABLE QUrl coverUrl() const;
+
+  qsizetype index() const;
+  Q_SIGNAL void indexChanged();
+
+  QUrl currentPage() const;
+  Q_INVOKABLE void nextPage();
+  Q_INVOKABLE void prevPage();
 
   static Ptr make(QString path, QObject* parent = nullptr);
 
  private:
   QString m_title;
+  qsizetype m_index = 0;
 };
 
 #endif
